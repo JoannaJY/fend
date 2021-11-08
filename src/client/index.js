ff1161d774d80ae4d3a101f40a322d46f2726fd9
Joanna YU <joannayu@Haos-iMac.local>
2021-11-08 21:17:34 +1030

    update server js

--- src/client/index.js
+++ src/client/index.js
@@ -8,5 +8,27 @@ import './styles/header.scss'
 
 console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1";
+
+document.getElementById('generate').addEventListener('click',generateContent);
+
+const formdata = new FormData();
+formdata.append("key", process.env.API_KEY);
+formdata.append("txt", "YOUR TEXT HERE");
+formdata.append("lang", en);  // 2-letter code, like en es fr ...
+
+const requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+const response = fetch(baseUrl, requestOptions)
+  .then(response => ({
+    status: response.status, 
+    body: response.json()
+  }))
+  .then(({ status, body }) => console.log(status, body))
+  .catch(error => console.log('error', error));
+
+
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,19 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+
+
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
@@ -15,15 +23,21 @@ console.log(__dirname)
 var textapi = new meaningCloud({
     application_key: process.env.API_KEY
   });
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
+// setup server
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
+})
+
+let textdata ={};
+
+app.post('/', function(req,res) {
+    textData = req.body;
+    return res.send(textData);
+})
+
+app.get('/', function (req, res) {
+    return res.send(textData);
+   // return res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 app.get('/test', function (req, res) {
