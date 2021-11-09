a0e47ef2ca23b06813fd49b66f11eec4fe77d225
Joanna YU <joannayu@Haos-iMac.local>
2021-11-09 20:07:50 +1030

    updata client index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,27 +1,70 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+console.log('hello' + checkForName);
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
+
 
 const baseUrl = "https://api.meaningcloud.com/sentiment-2.1";
 
-document.getElementById('generate').addEventListener('click',generateContent);
-
 const formdata = new FormData();
 formdata.append("key", process.env.API_KEY);
-formdata.append("txt", "YOUR TEXT HERE");
-formdata.append("lang", en);  // 2-letter code, like en es fr ...
+formdata.append("txt", document.getElementById('mytext').value);
+formdata.append("lang", en); 
 
-const requestOptions = {
-  method: 'POST',
-  body: formdata,
-  redirect: 'follow'
-};
+
+
+document.getElementById('generate').addEventListener('click',generateContent);
+
+function generateContent(){
+    console.log('clicked');
+    if (document.getElementById('mytext').value !=''){
+        getContent (baseUrl, sentimentData)
+    }else {
+        return "Please enter your text!"
+    }
+}
+
+function sentimentData (data) {
+   
+}
+
+const postText = async (url, formdata) => {
+    let base = 'http://localhost:8080'
+    const response = await fetch(base + url, {
+        method:'post',
+        credential: 'same-origin',
+        headers:{
+            'content-type': 'application/json',
+        },
+        body: formdata,
+        redirect: 'follow'
+    });
+    try {
+        const data = await response.json();
+        return data
+    } catch(error) {
+        console.log('error', error);
+    }
+}
+
+const  getContent = async (url='',data) => {
+    console.log('get content '+url);
+    const request = await fetch(url);
+    try {
+        const allData = await request.json()
+        console.log(allData);
+        data(allData);
+    } catch (error){
+        console.log('error',error)
+    }
+}
+
+
 
 const response = fetch(baseUrl, requestOptions)
   .then(response => ({
--- src/server/index.js
+++ src/server/index.js
@@ -6,40 +6,35 @@ const mockAPIResponse = require('./mockAPI.js')
 const dotenv = require('dotenv');
 dotenv.config();
 
+
+var textapi = new meaningCloud({
+    application_key: process.env.API_KEY
+});
+
 const app = express()
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const cors = require('cors');
-app.use(cors());
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
-//call meaning cloud API
-var textapi = new meaningCloud({
-    application_key: process.env.API_KEY
-  });
 // setup server
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-let textdata ={};
+let textData ={};
 
-app.post('/', function(req,res) {
+app.post('/text', function(req,res) {
     textData = req.body;
     return res.send(textData);
 })
 
-app.get('/', function (req, res) {
+app.get('/all', function (req, res) {
     return res.send(textData);
    // return res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
 })
